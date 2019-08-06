a006138e2c483e43a72a4e58dab95240f1042eda
Savepong <savepong.com@gmail.com>
2019-08-06 15:05:42 +0700

    Added: barcode scanner & http client

---
+++ doeScannerApp/src/app/home/home.page.ts
@@ -0,0 +1,76 @@
+import { Component } from "@angular/core";
+import { BarcodeScanner } from "@ionic-native/barcode-scanner/ngx";
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
+})
+export class HomePage {
+  constructor(
+    private barcodeScanner: BarcodeScanner,
+    private http: HttpClient
+  ) {
+    // this.getData();
+    this.startScan();
+  }
+
+  data: any = {
+    aliCancel: "",
+    ename: "",
+    fname: "",
+    lname: "",
+    masterCode: "",
+    masterName: "",
+    pid: "",
+    provinceNameReport: "",
+    return: "",
+    tlAuto: "",
+    tlDateInform: "", // 2019-04-30T00:00:00.000+07:00
+    tlDateWork: "",
+    tlTid: "",
+    tltype: ""
+  };
+
+  status = {
+    EN: "นายจ้างแจ้งเข้าสำเร็จ",
+    AN: "คนต่างด้าวแจ้งเข้าสำเร็จ",
+    EO: "นายจ้างแจ้งออกสำเร็จ"
+  };
+
+  laborStatus = {
+    "": "ปกติ",
+    C: "ถูกเพิกถอนใบอนุญาต"
+  };
+
+  startScan() {
+    this.barcodeScanner
+      .scan()
+      .then(barcodeData => {
+        // console.log(barcodeData);
+        this.getData(barcodeData.text);
+        console.log(this.data);
+        // this.startScan();
+      })
+      .catch(err => {
+        console.log("Error", err);
+      });
+  }
+
+  getData(id) {
+    this.http
+      .post("http://122.155.84.131/doe/doefilealien/selectcheckinformmobile", {
+        apikey: "ebefb44c35628c26848b6d71993994470fa24cff",
+        informid: id
+      })
+      .subscribe(
+        data => {
+          this.data = data;
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
+}
